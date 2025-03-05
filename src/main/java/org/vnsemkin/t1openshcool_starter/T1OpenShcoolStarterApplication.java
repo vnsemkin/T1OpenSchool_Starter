9463035cfe217b50c52566837a89950f29f4d7bc
Vladimir Semkin <vnsemkin@gmail.com>
2025-03-05 23:14:12 +0700

    Init commit

---
+++ src/main/java/org/vnsemkin/t1openshcool_starter/T1OpenShcoolStarterApplication.java
@@ -0,0 +1,13 @@
+package org.vnsemkin.t1openshcool_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1OpenShcoolStarterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(T1OpenShcoolStarterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/vnsemkin/t1openshcool_starter/T1OpenShcoolStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnsemkin.t1openshcool_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1OpenShcoolStarterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
