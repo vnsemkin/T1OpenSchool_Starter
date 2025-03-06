6d706aede98ab8e938404702998a8187565e62f4
Vladimir Semkin <vnsemkin@gmail.com>
2025-03-06 19:22:59 +0700

    Реализовано логирование вызовов контроллера через подключение стартера.

--- my_logging/src/main/java/org/vnsemkin/my_logging/config/AppHttpLoggingAutoConfiguration.java
+++ my_logging/src/main/java/org/vnsemkin/my_logging/config/AppHttpLoggingAutoConfiguration.java
@@ -1,12 +1,9 @@
 package org.vnsemkin.my_logging.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.vnsemkin.my_logging.aspect.AppHttpLoggingAspect;
 
 @Configuration
@@ -15,9 +12,7 @@ import org.vnsemkin.my_logging.aspect.AppHttpLoggingAspect;
     name = "enabled",
     havingValue = "true",
     matchIfMissing = true)
-@ConditionalOnClass(DispatcherServlet.class)
 @EnableConfigurationProperties(AppHttpLoggingProperties.class)
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class AppHttpLoggingAutoConfiguration {
 
   @Bean
